cba1e8a48c0751dfa8342035372a14eb3fd61e6b
Logan McAnsh <logan@mcan.sh>
2023-04-19 11:08:27 -0400

    chore!: remove deno and cloudflare
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- packages/remix-redis-session-storage/src/cloudflare.ts
+++
@@ -1,5 +0,0 @@
-import { createSessionStorage } from "@remix-run/cloudflare";
-import { createRedisSessionStorageFactory } from "./factory";
-
-export const createRedisSessionStorage =
-  createRedisSessionStorageFactory(createSessionStorage);
--- packages/remix-redis-session-storage/src/deno.ts
+++
@@ -1,5 +0,0 @@
-import { createSessionStorage } from "@remix-run/deno";
-import { createRedisSessionStorageFactory } from "./factory";
-
-export const createRedisSessionStorage =
-  createRedisSessionStorageFactory(createSessionStorage);
