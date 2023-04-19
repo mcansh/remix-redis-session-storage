75e8bb838efee6f46159ab904d06ff637467b8ae
Logan McAnsh <logan@mcan.sh>
2023-04-19 11:14:06 -0400

    chore!: remove deno and cloudflare (#3)
    * chore!: remove deno and cloudflare
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    * Create fluffy-numbers-rhyme.md
    
    * chore: update lockfile
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    ---------
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- packages/remix-redis-session-storage/tsup.config.ts
+++ packages/remix-redis-session-storage/tsup.config.ts
@@ -6,51 +6,25 @@ import pkgJSON from "./package.json";
 // @ts-expect-error
 let external = Object.keys(pkgJSON.dependencies || {});
 
-let shared_options: Options = {
-  sourcemap: true,
-  external,
-  tsconfig: "./tsconfig.json",
-};
+export default defineConfig(() => {
+  let shared_options: Options = {
+    sourcemap: true,
+    external,
+    tsconfig: "./tsconfig.json",
+  };
 
-function node(): Array<Options> {
   return [
     {
       ...shared_options,
-      entry: ["src/node.ts"],
+      entry: ["src/index.ts"],
       format: "cjs",
     },
 
     {
       ...shared_options,
-      entry: ["src/node.ts"],
+      entry: ["src/index.ts"],
       format: "esm",
       dts: true,
     },
   ];
-}
-
-function cloudflare(): Array<Options> {
-  return [
-    {
-      ...shared_options,
-      entry: ["src/cloudflare.ts"],
-      format: "esm",
-      dts: true,
-    },
-  ];
-}
-
-function deno(): Array<Options> {
-  return [
-    {
-      ...shared_options,
-      entry: ["src/deno.ts"],
-      format: "esm",
-      dts: true,
-    },
-  ];
-}
-
-export default defineConfig(() => {
-  return [...node(), ...cloudflare(), ...deno()];
 });
